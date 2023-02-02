7f9f1ac95214ed786768969c92f101767e32f923
Jordi <sroot079@gmail.com>
2023-02-01 23:02:22 -0500

    dice catastrophe

---
+++ CTFs/dice/catastrophe/exp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from pwn import *
+context.log_level='debug'
+context.arch='amd64'
+#context.terminal = ['tmux', 'splitw', '-h', '-F' '#{pane_pid}', '-P']
+p=process('./catastrophe')
+ru 		= lambda a: 	p.readuntil(a)
+r 		= lambda n:		p.read(n)
+sla 	= lambda a,b: 	p.sendlineafter(a,b)
+sa 		= lambda a,b: 	p.sendafter(a,b)
+sl		= lambda a: 	p.sendline(a)
+s 		= lambda a: 	p.send(a)
+gdb.attach(p)
+p.interactive()
