01fb4af334e42f7d29085e7351a8577ce71ae596
Jordi <sroot079@gmail.com>
2023-05-09 19:10:40 -0400

    Update exp.py

--- pwnable.kr/md5calculator/exp.py
+++ pwnable.kr/md5calculator/exp.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 from pwn import *
-context.log_level='debug'
-context.arch='amd64'
-#context.terminal = ['tmux', 'splitw', '-h', '-F' '#{pane_pid}', '-P']
-p=process('./pwn')
-ru 		= lambda a: 	p.readuntil(a)
-r 		= lambda n:		p.read(n)
-sla 	= lambda a,b: 	p.sendlineafter(a,b)
-sa 		= lambda a,b: 	p.sendafter(a,b)
-sl		= lambda a: 	p.sendline(a)
-s 		= lambda a: 	p.send(a)
+
+context.log_level = "debug"
+context.arch = "i386"
+context.terminal = ["tmux", "splitw", "-h", "-F" "#{pane_pid}", "-P"]
+# p=process('./hash')
+e = ELF("./hash")
+libc = ELF("/lib/x86_64-linux-gnu/libc-2.23.so")
+p = remote("pwnable.kr", 9002)
+ru = lambda a: p.readuntil(a)
+r = lambda n: p.read(n)
+sla = lambda a, b: p.sendlineafter(a, b)
+sa = lambda a, b: p.sendafter(a, b)
+sl = lambda a: p.sendline(a)
+s = lambda a: p.send(a)
+
 gdb.attach(p)
+
+ru(": ")
+captcha = p.recvline().decode()
+sl(captcha)
+
+
 p.interactive()
