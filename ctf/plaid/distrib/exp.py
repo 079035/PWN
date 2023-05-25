6255a0e29ecbe76aa85b67d2966e1293e0f15996
079035 <62355576+079035@users.noreply.github.com>
2023-05-25 00:00:28 -0500

    Update exp.py

--- ctf/plaid/distrib/exp.py
+++ ctf/plaid/distrib/exp.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 from pwn import *
+from binascii import hexlify
 context.log_level='debug'
-context.arch='amd64'
+context.arch='i386'
 #context.terminal = ['tmux', 'splitw', '-h', '-F' '#{pane_pid}', '-P']
-p=process('./pwn')
+
+p=process(['wine','./baby-heap-question-mark.exe'])
+
 ru 		= lambda a: 	p.readuntil(a)
 r 		= lambda n:		p.read(n)
 sla 	= lambda a,b: 	p.sendlineafter(a,b)
 sa 		= lambda a,b: 	p.sendafter(a,b)
 sl		= lambda a: 	p.sendline(a)
 s 		= lambda a: 	p.send(a)
-gdb.attach(p)
+# gdb.attach(p)
+
+
 p.interactive()
